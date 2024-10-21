e75e7a20be8d9338c8a7b0f9e9a7ea34c815d597
felippe.flip <felippe.flip@gmail.com>
2024-10-21 20:47:18 -0300

    v1.0.5

--- app/Http/Controllers/FuncoesController.php
+++ app/Http/Controllers/FuncoesController.php
@@ -15,49 +15,48 @@ class FuncoesController extends Controller
     }
 
     public function upload(Request $request)
-{
-    // Validação dos dados
-    $request->validate([
-        'category' => 'required|string',
-        'pdf' => 'required|file|mimes:pdf|max:2048', // Max 2 MB
-    ]);
+    {
+            // Validação dos dados
+            $request->validate([
+                'category' => 'required|string',
+                'html' => 'required|file|mimes:html|max:2048', // Max 2 MB, aceitando apenas HTML
+            ]);
 
     try {
-        // Processar o upload do arquivo
-        $pdf = $request->file('pdf');
-        $categoryName = $request->input('category');
+            // Processar o upload do arquivo
+            $htmlFile = $request->file('html');
+            $categoryName = $request->input('category');
 
-        // Formatando o nome do arquivo
-        $fileName = strtolower(str_replace(' ', '-', $categoryName)) . '.pdf';
+            // Formatando o nome do arquivo
+            $fileName = strtolower(str_replace(' ', '-', $categoryName)) . '.html';
 
-        // Definir o caminho para o diretório de uploads
-        $uploadPath = 'public/uploads';
+            // Definir o caminho para o diretório de uploads
+            $uploadPath = 'public/uploads';
 
-        // Verificar se o diretório de uploads existe, caso contrário, criar
-        if (!Storage::exists($uploadPath)) {
+            // Verificar se o diretório de uploads existe, caso contrário, criar
+            if (!Storage::exists($uploadPath)) {
             Storage::makeDirectory($uploadPath);
-        }
+            }
 
-        // Verificar se já existe um arquivo com o mesmo nome e excluí-lo
-        if (Storage::exists($uploadPath . '/' . $fileName)) {
-            Storage::delete($uploadPath . '/' . $fileName);
-        }
+            // Verificar se já existe um arquivo com o mesmo nome e excluí-lo
+            if (Storage::exists($uploadPath . '/' . $fileName)) {
+                Storage::delete($uploadPath . '/' . $fileName);
+            }
 
-        // Mover o arquivo para o diretório de uploads com o novo nome
-        $path = $pdf->storeAs($uploadPath, $fileName);
+            // Mover o arquivo para o diretório de uploads com o novo nome
+            $path = $htmlFile->storeAs($uploadPath, $fileName);
 
-        // Gerar a URL pública para o arquivo
-        $fileUrl = Storage::url($path);
+            // Gerar a URL pública para o arquivo
+            $fileUrl = Storage::url($path);
 
-        return redirect()->route('resultados.showImportForm')
-                         ->with('success', 'PDF importado com sucesso!')
+            return redirect()->route('resultados.showImportForm')
+                         ->with('success', 'Arquivo HTML importado com sucesso!')
                          ->with('fileUrl', $fileUrl);
-    } catch (\Exception $e) {
-        // Em caso de erro, redirecionar de volta com uma mensagem de erro
-        return redirect()->route('resultados.showImportForm')
-                         ->with('error', 'Ocorreu um erro ao importar o PDF. Por favor, tente novamente.');
+
+            } catch (\Exception $e) {
+            // Em caso de erro, redirecionar de volta com uma mensagem de erro
+            return redirect()->route('resultados.showImportForm')
+                         ->with('error', 'Ocorreu um erro ao importar o arquivo HTML. Por favor, tente novamente.');
+            }
     }
-}
-
-
 }
